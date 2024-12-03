f39204e0f085a35ea517799bc15c4ca944fec734
AdamNandor <144881211+AdamNandor@users.noreply.github.com>
2024-12-03 15:56:57 +0100

    szamlalo bug javitva

--- laci.py
+++ laci.py
@@ -9,9 +9,9 @@ for i in range(10):
     for j in range(10):
         sor.append(" ")
     matrix.append(sor)
-print("     1   2   3   4   5   6   7   8   9   10")
 def Megjelenit():
     szamlalo = 0
+    print("     1   2   3   4   5   6   7   8   9   10")
     print("   ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐")
     for i in matrix:
         szamlalo += 1
